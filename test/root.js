27efb470d08a603d6b687d7ec9c12b33f5106288
Jeff Astor <jastor@laalliance.org>
2017-02-28 20:02:16 +0000

    Fixed expect package.json issue.

--- test/root.js
+++ test/root.js
@@ -1,22 +1,15 @@
-global.expect = require('expect');
+global.expect = require('expect')
 
 const babel = require('babel-core');
 const jsdom = require('jsdom');
 const path = require('path');
 
 before(function(done) {
-  const babelResult = babel.transformFileSync(
-    path.resolve(__dirname, '..', 'index.js'), {
-      presets: ['es2015']
-    }
-  );
+  const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+    presets: ['es2015']
+  });
 
-  const html = path.resolve(__dirname, '..', 'index.html')
-
-  jsdom.env(html, [], {
-    src: babelResult.code,
-    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-  }, (err, window) => {
+  jsdom.env(path.resolve(__dirname, '..', 'index.html'), [], {src: babelResult.code}, (err, window) => {
     if (err) {
       return done(err);
     }
